111f47d579c2487b97ab2c7dc3f0305df7785905
Serg Pretender <sergdynamite@gmail.com>
2023-12-12 17:52:35 +0200

    Add template for post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import post_list
+from blog.views import post_list, post_detail
 
 urlpatterns = [
-    path('post/', post_list),
+    path('', post_list, name='home'),
+    path('post/<int:pid>/', post_detail, name='post_detail'),
 ]
