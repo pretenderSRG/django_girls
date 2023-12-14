0e940968ca411a1834fe88606e6591f01266d77c
Serg Pretender <sergdynamite@gmail.com>
2023-12-14 12:44:17 +0200

    Add create add edit form

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from blog.views import post_list, post_detail
+from blog.views import post_list, post_detail, post_new, post_edit, post_delete
 
 urlpatterns = [
     path('', post_list, name='home'),
-    path('post/<int:pid>/', post_detail, name='post_detail'),
+    path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('post/new/', post_new, name='post_new'),
+    path('post/<int:pk>/edit/', post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', post_delete, name='post_delete'),
 ]
